812e44d1a4b525f18fa1052cf225cb589225aa91
neerajkumar161 <ennkay161@gmail.com>
2022-06-01 14:17:53 +0530

    Recipes: Swagger(OpenApi)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats Example')
+    .setDescription('The Cats API Description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
